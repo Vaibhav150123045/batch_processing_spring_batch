269d7c2fae8faff9852c66c9565c5e0b169881c9
Vaibhav Motwani <motwani777@gmail.com>
2025-09-19 23:31:31 +0200

    added person model class

---
+++ src/main/java/com/example/batch/Person.java
@@ -0,0 +1,13 @@
+package com.example.batch;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    // Getters & Setters
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
