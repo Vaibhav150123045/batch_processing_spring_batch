e598b2101272f49bf8f61477b56a28871ef1879b
Vaibhav Motwani <motwani777@gmail.com>
2025-09-27 22:43:16 +0200

    added class for handling contact details objects

---
+++ src/main/java/com/example/batch/models/ContactDetails.java
@@ -0,0 +1,35 @@
+package com.example.batch;
+
+public class ContactDetails {
+    private String phoneNumber;
+    private String email;
+    private String alternatePhone;
+
+    // Constructors
+    public ContactDetails() {}
+
+    public ContactDetails(String phoneNumber, String email, String alternatePhone) {
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.alternatePhone = alternatePhone;
+    }
+
+    // Getters & Setters
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getAlternatePhone() { return alternatePhone; }
+    public void setAlternatePhone(String alternatePhone) { this.alternatePhone = alternatePhone; }
+
+    @Override
+    public String toString() {
+        return "ContactDetails{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", alternatePhone='" + alternatePhone + '\'' +
+                '}';
+    }
+}
