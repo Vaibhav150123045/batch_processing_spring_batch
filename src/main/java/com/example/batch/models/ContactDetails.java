c9e1b6ce41e7cdea1267a7d9481602cd245e7e43
Vaibhav Motwani <motwani777@gmail.com>
2025-09-30 09:18:35 +0200

    added contact details model class

--- src/main/java/com/example/batch/models/ContactDetails.java
+++ src/main/java/com/example/batch/models/ContactDetails.java
@@ -1,4 +1,4 @@
-package com.example.batch;
+package com.example.batch.models;
 
 public class ContactDetails {
     private String phoneNumber;
@@ -7,7 +7,6 @@ public class ContactDetails {
 
     // Constructors
     public ContactDetails() {}
-
     public ContactDetails(String phoneNumber, String email, String alternatePhone) {
         this.phoneNumber = phoneNumber;
         this.email = email;
@@ -15,21 +14,4 @@ public class ContactDetails {
     }
 
     // Getters & Setters
-    public String getPhoneNumber() { return phoneNumber; }
-    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getAlternatePhone() { return alternatePhone; }
-    public void setAlternatePhone(String alternatePhone) { this.alternatePhone = alternatePhone; }
-
-    @Override
-    public String toString() {
-        return "ContactDetails{" +
-                "phoneNumber='" + phoneNumber + '\'' +
-                ", email='" + email + '\'' +
-                ", alternatePhone='" + alternatePhone + '\'' +
-                '}';
-    }
 }
